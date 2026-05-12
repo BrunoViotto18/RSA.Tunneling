4101c8eb37e783ce562c18a70785f3cbb5d19271
Bruno Viotto dos Santos <Bruno.Santos03@cs.up.edu.br>
2026-05-12 00:03:57 -0300

    feat: Create sign and auth with SHA256

---
+++ src/Viotto.Security.AuthSign/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Text;
+using Viotto.Security;
+
+if (args.Length < 2 || args[0] is not "sign" and not "auth")
+{
+    throw new ArgumentException("Argumentos desconhecidos ou inálidos");
+}
+
+var sha256Hasher = new Sha256Hasher();
+var base64Encoder = new Base64Encoder();
+
+var filePath = args[1];
+
+var fileContent = await File.ReadAllTextAsync(filePath);
+
+var bytes = Encoding.UTF8.GetBytes(fileContent);
+
+var hashedBytes = sha256Hasher.Hash(bytes);
+
+var hash = base64Encoder.ToBase64(hashedBytes);
+
+if (args[0] is "sign")
+{
+    Console.WriteLine($"Assinatura SHA256/base64 do arquivo: {hash}");
+}
+else if (args[0] is "auth")
+{
+    Console.Write("Digite a assinatura do arquivo [SHA256/base64]: ");
+    var signature = Console.ReadLine();
+
+    if (signature == hash)
+    {
+        Console.WriteLine("O arquivo é autêntico!");
+    }
+    else
+    {
+        Console.WriteLine("O arquivo não é autêntico, ou pode ter sido modificado!");
+    }
+}
